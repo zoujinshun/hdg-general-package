d59bf064617866d3266abf441f2cef02bc16d2de
zjs <dlyzjs@163.com>
2022-06-10 15:55:08 +0800

    update

--- src/Providers/Helper.php
+++ src/Providers/Helper.php
@@ -4,15 +4,16 @@ namespace Vaedly\HdgGeneralPackage\Providers;
 
 use Vaedly\HdgGeneralPackage\Repository\Repository;
 use Illuminate\Support\Facades\Redis;
+use Illuminate\Support\Arr;
 
 class Helper
 {
     public function responseProvider($array)
     {
         return response()->json([
-            'status' => array_get($array, 0),
-            'data' => array_get($array, 1),
-            'message' => array_get($array, 2, '')
+            'status' => Arr::get($array, 0),
+            'data' => Arr::get($array, 1),
+            'message' => Arr::get($array, 2, '')
         ]);
     }
 
@@ -21,7 +22,7 @@ class Helper
         $redis_key = "domain_list";
         if (Redis::get($redis_key)) {
             $domain_list = json_decode(Redis::get($redis_key), true);
-            return array_get($domain_list, $key . '.url');
+            return Arr::get($domain_list, $key . '.url');
         }
 
         $repository = new Repository();
@@ -33,6 +34,6 @@ class Helper
         }
 
         Redis::setex($redis_key, 86400, json_encode($list));
-        return array_get($list, $key . '.url');
+        return Arr::get($list, $key . '.url');
     }
 }
